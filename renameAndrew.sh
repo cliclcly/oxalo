819d75f76acec489cbfdc944a078d11d34b6ecfe
Andrew McCurry <mdandrewm@gmail.com>
2012-08-09 00:59:52 -0400

    makefile

--- renameAndrew.sh
+++
@@ -1,8 +0,0 @@
-git filter-branch --env-filter '
-      if [ "$GIT_COMMITTER_NAME" = "unknown" ];
-      then
-          export GIT_AUTHOR_EMAIL="mdandrewm@gmail.com";
-          export GIT_AUTHOR_NAME="Andrew McCurry";
-          export GIT_COMMITTER_EMAIL="mdandrewm@gmail.com";
-          export GIT_COMMITTER_NAME="Andrew McCurry";
-      fi' HEAD
